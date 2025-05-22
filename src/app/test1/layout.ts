50f6f6b5361633bbd6302744aafc6beab3c661cc
Kirigaya-Igor <ighorshian0@gmail.com>
2025-05-22 13:04:52 +0200

    new update

--- src/app/test1/layout.ts
+++ src/app/test1/layout.ts
@@ -4,15 +4,14 @@ export async function generateMetadata() {
         description: 'Test1',
         openGraph: {
             title: 'Test1 title',
-            images: 'https://is-next-seo.netlify.app/test1/opengraph-image',
             // images: 'https://is-next-seo.netlify.app/test1.jpg',
         },
         twitter: {
             title: 'Test1 title',
-            images: 'https://is-next-seo.netlify.app/test1/opengraph-image',
             // images: 'https://is-next-seo.netlify.app/test1.jpg',
             card: 'summary_large_image',
         },
+        metadataBase: new URL('https://is-next-seo.netlify.app'),
     };
 }
 
