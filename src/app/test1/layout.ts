de7f78d4d6b15d2b42d5cc18be11d509abd2861c
Kirigaya-Igor <ighorshian0@gmail.com>
2025-05-22 12:41:53 +0200

    new update

--- src/app/test1/layout.ts
+++ src/app/test1/layout.ts
@@ -2,15 +2,17 @@ export async function generateMetadata() {
     return {
         title: 'Test1 Title',
         description: 'Test1',
-        // openGraph: {
-        //     title: 'Test1',
-        //     images: 'https://is-next-seo.netlify.app/test1.jpg',
-        // },
-        // twitter: {
-        //     title: 'Test1',
-        //     images: 'https://is-next-seo.netlify.app/test1.jpg',
-        //     card: 'summary_large_image',
-        // },
+        openGraph: {
+            title: 'Test1',
+            images: 'https://is-next-seo.netlify.app/test1/opengraph-image',
+            // images: 'https://is-next-seo.netlify.app/test1.jpg',
+        },
+        twitter: {
+            title: 'Test1',
+            images: 'https://is-next-seo.netlify.app/test1/opengraph-image',
+            // images: 'https://is-next-seo.netlify.app/test1.jpg',
+            card: 'summary_large_image',
+        },
     };
 }
 
