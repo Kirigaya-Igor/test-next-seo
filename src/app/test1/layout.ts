2033781a353682e87c0117e0ae3d0b179c678b9a
Kirigaya-Igor <ighorshian0@gmail.com>
2025-06-03 12:04:59 +0200

    new update

--- src/app/test1/layout.ts
+++ src/app/test1/layout.ts
@@ -1,17 +1,13 @@
 export async function generateMetadata() {
-    const imageUrl = 'https://is-next-seo.netlify.app/test1/opengraph-image.tsx';
-
     return {
         metadataBase: new URL('https://is-next-seo.netlify.app'),
         title: 'Test1 title',
         openGraph: {
             title: 'Test1 title',
-            images: [imageUrl],
         },
         twitter: {
-            card: 'summary_large_image',
             title: 'Test1 title',
-            images: [imageUrl],
+            card: 'summary_large_image',
         },
     };
 }
